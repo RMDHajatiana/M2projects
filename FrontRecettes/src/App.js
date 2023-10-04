125a96940539c26c087b3b2cbc93806ba58c7d4d
Hajatiana <nente07@gmail.com>
2023-10-04 23:57:09 +0300

    Modificationde style dans le front

--- FrontRecettes/src/App.js
+++ FrontRecettes/src/App.js
@@ -1,15 +1,59 @@
-import React, {  } from "react";
-//import './Styles/App.css';
-// import Appfooter from "./Components/Appfooter";
-// import AppContent from "./Components/AppContent";
-// import Menus from "./Components/Menus";
-// import Header from "./Components/Header";
+import React, { useState } from "react";
+import { Drawer } from "antd";
+//import {LogoutOutlined, MenuOutlined} from "@ant-design/icons";
+import Menus from "./Components/Menus"
+import Menus2 from "./Components/Menus2";
+import AppContent from "./Components/AppContent";
+import Appfooter from "./Components/Appfooter"
+import * as AiIcons from "react-icons/ai";
+import './Styles/App.css';
 
 function App() {
 
-  return (
-    <div className="App">
+  const [openMenu, SetOpenMenu] = useState(false)
 
+  const HandleClose = () => {
+    SetOpenMenu(false)
+  }
+  
+  const HandleOpen = () => {
+    SetOpenMenu(true)
+  }
+  return (
+
+    <div className="MenusContent" >
+
+      <div 
+      className="menuIcon"
+      style={{ backgroundColor:'white', color:'black',  height: 50,  paddingLeft:12, paddingTop:12, }}>
+
+        <AiIcons.AiOutlineMenuUnfold
+        onClick={HandleOpen}
+        style={{ color: 'Black',fontSize : 24,
+        }} />
+      {/* <Menu
+      mode="horizontal"
+        style={{ right:200, flexDirection:'column' }}
+      items={[
+        {label:'Deconnexion', icon: <LogoutOutlined/>, danger: true ,  }
+      ]} 
+      ></Menu> */}
+      </div>
+
+      <span className="headerMenu">
+     <Menus2/>
+      </span>
+
+      <AppContent/>
+      <Appfooter/>
+
+     <Drawer 
+          placement = 'left'
+          open ={openMenu}  
+          closable={false} 
+          onClose={HandleClose}>
+          <Menus Isinline />
+     </Drawer>
     </div>
   );
 }
--- FrontRecettes/src/Components/Menus2.jsx
+++ FrontRecettes/src/Components/Menus2.jsx
@@ -4,28 +4,30 @@ import { useNavigate } from 'react-router-dom';
 import { 
     AccountBookOutlined,
      DashboardOutlined,
-     LogoutOutlined, 
     } from '@ant-design/icons';
 import * as MdIcons from "react-icons/md";
 import * as FaIcons from "react-icons/fa";
 
 
-const Menus2 = () => {
+const Menus2 = ({Isinline= false}) => {
 
         const Navigate = useNavigate ()
         return (
             <div  className='Menus2' >
             <Menu
-            mode='horizontal'
+
+            mode=  { Isinline ?   "inline": "horizontal" }
+
+            style={{ color:'black',  fontSize: 16  }}
+
             onClick={({key}) => {
-               Navigate(key)
-            }}
+                    Navigate(key)
+                    }}
             items={[
-           { label : 'Tableau de Bord', key : '/', icon: <DashboardOutlined/> },
+         { label : 'Tableau de Bord', key : '/', icon: <DashboardOutlined/> },
            {label : 'Vente', key : 'Vente',  icon: <FaIcons.FaShopify/>},
            {  label : 'Recettes',key : 'Recettes',  icon: <AccountBookOutlined/>},
            {label : 'Nos Vols', key : 'Vols',  icon:<MdIcons.MdOutlineFlightTakeoff/>},
-           {  label : 'Deconnexion', key : 'Deconnexion',  icon: <LogoutOutlined/>, danger:true,  },
         ]} >
         </Menu>
         </div>
